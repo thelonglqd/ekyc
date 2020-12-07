70d9ee2dcff646199a1b1e02e9ef162b500aa1e9
Long Nguyen The Phung <thelonglqd@gmail.com>
2020-12-07 23:48:50 +0700

    setup header, drawer, login and logout feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,258 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Drawer,
+  AppBar,
+  Toolbar,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+  Grid,
+  Paper,
+  Button,
+  TextField,
+} from "@material-ui/core";
 
-function App() {
+import {
+  Call,
+  Person,
+  Group,
+  Settings,
+  AccountCircle,
+} from "@material-ui/icons";
+
+const drawerWidth = 240;
+
+const routes = [
+  { icon: <Call />, text: "Quản lý cuộc gọi" },
+  { icon: <Person />, text: "Quản lý tài khoản khách hàng" },
+  { icon: <Group />, text: "Quản lý nhóm" },
+  { icon: <Settings />, text: "Quản lý tham số cấu hình hệ thống" },
+];
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    padding: 20,
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    zIndex: 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  title: {
+    flexGrow: 1,
+  },
+  username: {
+    marginRight: 5,
+  },
+  loginTitle: {
+    marginBottom: theme.spacing(4),
+  },
+  loginButton: {
+    marginTop: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(false);
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const onUsernameChange = (e) => setUsername(e.target.value);
+  const onPasswordChange = (e) => setPassword(e.target.value);
+
+  const onLoginClick = () => {
+    if (username === "admin" && password === "admin") setAuth(true);
+    else return;
+  };
+
+  const onLogoutClick = () => {
+    setAnchorEl(null);
+    setAuth(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      {auth ? (
+        <>
+          <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+              <Typography variant="h6" className={classes.title}>
+                Quản lý cuộc gọi
+              </Typography>
+              <div>
+                <IconButton
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                >
+                  <Typography className={classes.username} variant="h6">
+                    admin
+                  </Typography>
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>Đổi mật khẩu</MenuItem>
+                  <MenuItem onClick={handleClose}>Đổi avatar</MenuItem>
+                  <MenuItem onClick={onLogoutClick}>Đăng xuất</MenuItem>
+                </Menu>
+              </div>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            anchor="left"
+          >
+            <div className={classes.toolbar} />
+            <List>
+              {routes.map((route) => (
+                <ListItem button key={route.text}>
+                  <ListItemIcon>{route.icon}</ListItemIcon>
+                  <ListItemText primary={route.text} />
+                </ListItem>
+              ))}
+            </List>
+          </Drawer>
+          <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Typography paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Rhoncus dolor purus non enim praesent elementum facilisis leo vel.
+              Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+              gravida rutrum quisque non tellus. Convallis convallis tellus id
+              interdum velit laoreet id donec ultrices. Odio morbi quis commodo
+              odio aenean sed adipiscing. Amet nisl suscipit adipiscing bibendum
+              est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+              Metus vulputate eu scelerisque felis imperdiet proin fermentum
+              leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt
+              lobortis feugiat vivamus at augue. At augue eget arcu dictum
+              varius duis at consectetur lorem. Velit sed ullamcorper morbi
+              tincidunt. Lorem donec massa sapien faucibus et molestie ac.
+            </Typography>
+            <Typography paragraph>
+              Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+              ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+              elementum integer enim neque volutpat ac tincidunt. Ornare
+              suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
+              volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
+              Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
+              ornare massa eget egestas purus viverra accumsan in. In hendrerit
+              gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
+              aliquam sem et tortor. Habitant morbi tristique senectus et.
+              Adipiscing elit duis tristique sollicitudin nibh sit. Ornare
+              aenean euismod elementum nisi quis eleifend. Commodo viverra
+              maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin
+              aliquam ultrices sagittis orci a.
+            </Typography>
+          </main>
+        </>
+      ) : (
+        <Grid justify="center" container spacing={10}>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <Typography
+                color="primary"
+                variant="h5"
+                className={classes.loginTitle}
+              >
+                ĐĂNG NHẬP
+              </Typography>
+              <TextField
+                value={username}
+                onChange={onUsernameChange}
+                variant="outlined"
+                id="standard-full-width"
+                label="Tài khoản đăng nhập"
+                style={{ margin: 8 }}
+                fullWidth
+                margin="normal"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <TextField
+                type="password"
+                value={password}
+                onChange={onPasswordChange}
+                variant="outlined"
+                id="standard-full-width"
+                label="Mật khẩu"
+                style={{ margin: 8 }}
+                fullWidth
+                margin="normal"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <Button
+                className={classes.loginButton}
+                variant="contained"
+                color="primary"
+                onClick={onLoginClick}
+              >
+                ĐĂNG NHẬP
+              </Button>
+            </Paper>
+          </Grid>
+        </Grid>
+      )}
     </div>
   );
-}
-
+};
 export default App;
